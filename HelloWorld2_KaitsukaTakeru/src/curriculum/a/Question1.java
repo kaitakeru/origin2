2b2c8a7933561bf5b19ea592fba3198a090fa644
Takeru <Takeru@192.168.10.108>
2026-03-08 22:44:15 +0900

    課題2.10 Q12

--- HelloWorld2_KaitsukaTakeru/src/curriculum/a/Question1.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/a/Question1.java
@@ -3,17 +3,12 @@ package curriculum.a;
 public class Question1 {
 
 	public static void main(String[] args) {
-		// 1. int型の変数 a に 10 を代入
-        int a = 10;
-        
-        // 2. int型の変数 b に 20 を代入
-        int b = 20;
+		// 1. int 型の変数 x に 15 を代入
+        int x = 15;
 
-        // 3. a が b より小さいかどうかを条件演算子を使って判定し、result に代入
-        // 書式: 条件式 ? 真の場合の値 : 偽の場合の値
-        boolean result = (a < b) ? true : false;
+        // 2. x が 10 以上なら "OK"、そうでなければ "NG" を判定して表示
+        String message = (x >= 10) ? "OK" : "NG";
 
-        // 4. result の値を表示
-        System.out.println("a < b の結果: " + result);
+        System.out.println(message);	
 	}
 }
