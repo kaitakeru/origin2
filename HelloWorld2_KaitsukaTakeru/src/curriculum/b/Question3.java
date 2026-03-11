9dbe10b3e4d1601ab4f659389c75dd093616c936
Takeru <Takeru@192.168.10.108>
2026-03-11 11:09:53 +0900

    課題2.14 Q6

---
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question3.java
@@ -0,0 +1,13 @@
+package curriculum.b;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		int i = 1; //スタート
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++; // 数字を1増やす
+        }
+	}
+}
