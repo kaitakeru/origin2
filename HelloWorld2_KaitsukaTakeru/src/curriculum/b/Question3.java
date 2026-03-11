c5ad2bfa5b96412349e00b767c5e871571bdf5cf
Takeru <Takeru@192.168.10.108>
2026-03-11 11:19:23 +0900

    課題2.14 Q10

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question3.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question3.java
@@ -1,16 +1,25 @@
 package curriculum.b;
 
+import java.util.Scanner; // Scannerを使う
+
 public class Question3 {
 
 	public static void main(String[] args) {
-		int sum = 0; // 合計を入れるための箱
-        int i = 1;   // 1からスタート
+		// Scannerの準備
+        Scanner scanner = new Scanner(System.in);
+        
+        int input = -1;
 
-        while (i <= 100) {
-            sum += i; 
-            i++;
+        System.out.println("数字を入力してください（0で終了）:");
+
+        while (input != 0) {
+            System.out.print("入力 > ");
+            input = scanner.nextInt();
         }
 
-        System.out.println("1から100までの合計は " + sum + " です。");
+        System.out.println("終了しました");
+        
+        // Scannerを閉じる
+        scanner.close();
         }
 	}
