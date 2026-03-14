ed0ae75efaf32ecd5e1c95a94c237050896eed23
Takeru <Takeru@192.168.10.108>
2026-03-14 09:46:36 +0900

    課題2.17 Q8

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question4.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question4.java
@@ -4,13 +4,23 @@ public class Question4 {
 
 	public static void main(String[] args) {
 		// 1. 2次元配列を用意
-        int[][] array = {{1, 2}, {3, 4}, {5, 6}};
+        int[][] array = {
+            {10, 20, 30},
+            {40, 50, 60},
+            {70, 80, 90}
+        };
         
-        // 2. for文で全要素を表示
-        for (int i = 0; i < array.length; i++) {         
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.println("array[" + i + "][" + j + "] = " + array[i][j]);
+        // 2. 合計値を入れる変数を用意
+        int totalSum = 0;
+        
+        // 3. 拡張for文で全て足す
+        for (int[] row : array) {      // 各「行」を取り出す
+            for (int num : row) {      // 行の中の各要素を取り出す
+                totalSum += num;       // 合計に加算
             }
         }
-    }
-}
+        
+        // 4. 結果を表示
+        System.out.println("すべての要素の合計値は: " + totalSum);
+            }
+	}
