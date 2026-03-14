d905b3f2fece12d21cb39ec3ba50b4e42abc2d83
Takeru <Takeru@192.168.10.108>
2026-03-14 09:53:58 +0900

    課題2.17 Q10

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question4.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question4.java
@@ -3,33 +3,27 @@ package curriculum.b;
 public class Question4 {
 
 	public static void main(String[] args) {
-		// 1. 2次元配列を用意
-        int[][] array = {
-            {12, 15, 8},
-            {6, 19, 25},
-            {30, 2, 10}
+		// 1. 3次元配列を用意
+        int[][][] array = {
+            {
+                {1, 2}, 
+                {3, 4}
+            }, 
+            {
+                {5, 6}, 
+                {7, 8}
+            }
         };
 
-        // 2. 最大値と最小値を、（[0][0]）でセット
-        int max = array[0][0];
-        int min = array[0][0];
-
-        // 3. 拡張for文で全要素を比較
-        for (int[] row : array) {      // 各行を取り出す
-            for (int num : row) {      // 行の中の各要素を取り出す
-                // 最大値
-                if (num > max) {
-                    max = num;
+        // 2. 拡張for文で全要素を表示
+        for (int[][] area : array) {
+            for (int[] row : area) {
+                for (int num : row) {
+                    System.out.print(num + " ");
                 }
-                // 最小値
-                if (num < min) {
-                    min = num;
-                }
+                System.out.println(); // 行が終わったら改行
             }
+            System.out.println("----"); //区切り
         }
-
-        // 4. 結果表示
-        System.out.println("最大値は: " + max);
-        System.out.println("最小値は: " + min);
-            }
 	}
+}
