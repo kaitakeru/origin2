0422931d9c39acddc9098a2ab86e45a25d10b24d
Takeru <Takeru@192.168.10.108>
2026-03-16 17:59:47 +0900

    Q2 Q3

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question2.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question2.java
@@ -3,13 +3,16 @@ package curriculum.b;
 public class Question2 {
 
 	public static void main(String[] args) {
-		int age = 25; 
+		int age = 18;
 
-		if (age >= 20 && age <= 30) {
-		    System.out.println("適正年齢です");
-		} 
-		else {
-		    System.out.println("対象外です");
+		if (age >= 20) {
+		    System.out.println("成人です");
+		
+		} else if (age >= 13) {
+		    System.out.println("ティーンエイジャーです");
+		
+		} else {
+		    System.out.println("子供です");
 			}
 		}
 	}
