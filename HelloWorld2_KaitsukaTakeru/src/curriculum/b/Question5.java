e3feba58cddf5177d59dfa2635d09dd23fd2e28c
Takeru <Takeru@192.168.10.108>
2026-03-16 16:51:14 +0900

    課題2.21 Q2

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
@@ -1,13 +1,30 @@
 package curriculum.b;
 
+import java.util.Scanner;
+
 public class Question5 {
 
 	public static void main(String[] args) {
-		helloWorld(); // メソッド
+		// Scannerの準備
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("整数を入力してください: ");
+        // 整数を入力
+        int input = scanner.nextInt();
+        
+        // メソッド呼び出し
+        int result = doubleValue(input);
+        
+        // 実行例のように表示
+        System.out.println(input + " を 2 倍すると " + result + " です。");
+        
+        scanner.close();
     }
 
-    // メソッド helloWorld
-    public static void helloWorld() {
-        System.out.println("Hello, World!");
+    /**
+     * 引数の整数を2倍するメソッド
+     */
+    public static int doubleValue(int num) {
+        return num * 2;
 	}
 }
