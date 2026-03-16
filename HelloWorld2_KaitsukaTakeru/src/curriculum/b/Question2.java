7adb26e39b9ce9f0d02c30da4aa86732dbc9ac29
Takeru <Takeru@192.168.10.108>
2026-03-16 17:54:33 +0900

    Q2 Q1

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question2.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question2.java
@@ -1,42 +1,12 @@
 package curriculum.b;
 
-import java.util.Scanner;
-
 public class Question2 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-        
-		System.out.print("月を入力してください (1〜12): ");
-        int month = scanner.nextInt();
-        
-        // Switchで複数のcaseをまとめる
-        switch (month) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("冬");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("春");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("夏");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("秋");
-                break;
-            default:
-                System.out.println("無効な月です");
-                break;
-        }
-        
-        scanner.close();
+		int score = 75; 
+
+		if (score >= 60) {
+		    System.out.println("合格です！"); 
 		}
 	}
+}
