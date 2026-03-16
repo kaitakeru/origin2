8d1692120bb7560e75f84978fcfc85e3aa46ee80
Takeru <Takeru@192.168.10.108>
2026-03-16 16:56:35 +0900

    課題2.21 Q3

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
@@ -5,20 +5,35 @@ import java.util.Scanner;
 public class Question5 {
 
 	public static void main(String[] args) {
-		// Scannerは直接使わない
+		// Scannerの準備（固定値を使用）
         Scanner scanner = new Scanner(System.in);
         
-        // メソッド
-        helloWorld();
+        // 変数の設定
+        int num1 = 7;
+        int num2 = 10;
+        
+        // num2 (10) の判定と表示
+        if (isEven(num2)) {
+            System.out.println(num2 + " は偶数です。");
+        } else {
+            System.out.println(num2 + " は奇数です。");
+        }
+        
+        // num1 (7) の判定と表示
+        if (isEven(num1)) {
+            System.out.println(num1 + " は偶数です。");
+        } else {
+            System.out.println(num1 + " は奇数です。");
+        }
         
         scanner.close();
     }
 
     /**
-     * 条件：引数なし、戻り値なし
-     * "Hello, World!" を表示する
+     * 引数が偶数かを判定するメソッド
      */
-    public static void helloWorld() {
-        System.out.println("Hello, World!");
+    public static boolean isEven(int num) {
+        // 2で割った余りが0なら偶数
+        return num % 2 == 0;
 	}
 }
