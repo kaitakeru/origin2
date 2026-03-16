0cf71611104acddb9b10f7b329f7e83b86055192
Takeru <Takeru@192.168.10.108>
2026-03-16 18:05:55 +0900

    Q2 Q5

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question2.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question2.java
@@ -7,26 +7,19 @@ public class Question2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-        System.out.print("1つ目の数値を入力してください: ");
-        int x = sc.nextInt(); // 3. 入力を受け取る
+		System.out.print("数値を入力してください: ");
+        // 1. コンソール入力から数値を受け取る
+        int num = sc.nextInt();
 
-        System.out.print("2つ目の数値を入力してください: ");
-        int y = sc.nextInt();
-
-        System.out.print("3つ目の数値を入力してください: ");
-        int z = sc.nextInt();
-
-        // 最大値の判定ロジック
-        int max = x;
-        if (y > max) {
-            max = y;
+        // 2. 条件分岐で判定
+        if (num > 0) {
+            System.out.println("正の数です");
+        } else if (num == 0) {
+            System.out.println("0 です");
+        } else {
+            // 0より大きいでもなく、0でもない場合（＝0より小さい）
+            System.out.println("負の数です");
         }
-        if (z > max) {
-            max = z;
-        }
-
-        System.out.println("一番大きい数値は " + max + " です");
-
         sc.close();
 		}
 	}
