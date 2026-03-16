97dee93810b37359f8891f409f1460ef56d5cab7
Takeru <Takeru@192.168.10.108>
2026-03-16 16:43:28 +0900

    課題2.21 Q1

---
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
@@ -0,0 +1,13 @@
+package curriculum.b;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		helloWorld(); // メソッド
+    }
+
+    // メソッド helloWorld
+    public static void helloWorld() {
+        System.out.println("Hello, World!");
+	}
+}
