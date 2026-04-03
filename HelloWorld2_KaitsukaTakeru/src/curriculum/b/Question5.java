304efcd80ce87707b40321151ff845dbc0c5bcff
Takeru <Takeru@192.168.10.108>
2026-04-03 14:53:42 +0900

    Question5 260403

--- HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/b/Question5.java
@@ -1,39 +1,57 @@
 package curriculum.b;
 
-import java.util.Scanner;
-
 public class Question5 {
 
-	public static void main(String[] args) {
-		// Scannerの準備（固定値を使用）
-        Scanner scanner = new Scanner(System.in);
-        
-        // 変数の設定
+    public void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    public int doubleValue(int num) {
+        return num * 2;
+    }
+
+    public boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Question5 q = new Question5();
+
+        // Q1
+        q.helloWorld();
+
+        // Q2
+        int input = 10;
+        int result = q.doubleValue(input);
+        System.out.println(input + " を 2 倍すると " + result + " です。");
+
+        // Q3
         int num1 = 7;
         int num2 = 10;
-        
-        // num2 (10) の判定と表示
-        if (isEven(num2)) {
+        if (q.isEven(num2)) {
             System.out.println(num2 + " は偶数です。");
         } else {
             System.out.println(num2 + " は奇数です。");
         }
-        
-        // num1 (7) の判定と表示
-        if (isEven(num1)) {
+        if (q.isEven(num1)) {
             System.out.println(num1 + " は偶数です。");
         } else {
             System.out.println(num1 + " は奇数です。");
         }
-        
-        scanner.close();
-    }
 
-    /**
-     * 引数が偶数かを判定するメソッド
-     */
-    public static boolean isEven(int num) {
-        // 2で割った余りが0なら偶数
-        return num % 2 == 0;
-	}
+        // Q4
+        Greeting greeting = new Greeting();
+        greeting.sayHello();
+
+        // Q5
+        Animal lion = new Animal();
+        lion.setName("ライオン");
+        lion.setLength("2.1m");
+        lion.setSpeed("80km/h");
+
+        System.out.println("動物名：" + lion.getName());
+        System.out.println("体長：" + lion.getLength());
+        System.out.println("速度：" + lion.getSpeed());
+
+    }
 }
