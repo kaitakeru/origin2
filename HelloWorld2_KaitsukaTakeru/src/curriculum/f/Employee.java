340b5d408839c18fe27799b2f9125da08e954548
Takeru <Takeru@192.168.10.108>
2026-04-03 19:24:42 +0900

    オブジェクト指向② 260403

---
+++ HelloWorld2_KaitsukaTakeru/src/curriculum/f/Employee.java
@@ -0,0 +1,11 @@
+package curriculum.f;
+
+public class Employee {
+
+    public String employeeId;
+    public String name;
+
+    public void showInfo() {
+        System.out.println("社員ID: " + this.employeeId + ", 名前: " + this.name);
+    }
+}
